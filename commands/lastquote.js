f0803d247db936421192d566cad198b33ba9ee11
Someone Someone <Anooxi72@gmail.com>
2020-08-04 01:53:40 +0200

    last quote

---
+++ commands/lastquote.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+exports.run = async (client,message,args) => {
+    fs.readFile('./quotes.json', 'utf8', function readFileCallback(err, data){
+        if(err){
+            console.log(err);
+            message.channel.send("oskur y'a une erreur :(");
+        } else {
+            obj = JSON.parse(data);
+            let quotenumber = obj.numberofquotes;
+
+            message.channel.send("> " + obj.table[quotenumber].quote + "\n" + "**" + obj.table[quotenumber].nickname + "**" + " - " + obj.table[quotenumber].year);
+        }
+    });
+}
